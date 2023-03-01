95a17dafe12bb4d4361a75cf05fb1d936b6e15b0
limberMay <limbermay1@gmail.com>
2023-03-01 14:48:20 -0600

    clase SymbolTable eliminado

--- models/SymbolTable.js
+++ models/SymbolTable.js
@@ -2,9 +2,6 @@
 const { DataTypes } = require('sequelize');
 const db = require('../db/connection');
 
-// / -> localhost:3000
-// /user
-
 /*
     Symbol Table required attributes:
     - name
@@ -49,26 +46,3 @@ const SymbolTableSchema = db.define('SymbolTableSchema', {
 }, {
     tableName: 'symbolTable'
 });
-
-class SymbolTable {
-    constructor() {
-        this.table = {};
-    }
-
-    addSymbol(name, type, value, scope) {
-        // logic to add a symbol to the table
-    }
-}
-
-/* Example of a symbol
-    const number = 15;
- */
-
-// add the symbol to the table
-// name: number
-// type: int
-// value: 15
-// scope: global
-
-const tabla = new SymbolTable();
-tabla.addSymbol('number', 'int', 15, 'global');
