54044977f8483374866b308e528badd50ad023a4
limberMay <limbermay1@gmail.com>
2023-03-01 14:38:23 -0600

    Modelo de base de datos agregado

--- models/SymbolTable.js
+++ models/SymbolTable.js
@@ -1,11 +1,53 @@
 // noinspection SpellCheckingInspection
+const { DataTypes } = require('sequelize');
 const db = require('../db/connection');
 
 // / -> localhost:3000
 // /user
 
-const SymbolTableSchema = db.define('SymbolTableSchema', {
+/*
+    Symbol Table required attributes:
+    - name
+    - dataType
+    - type
+    - scope
+    - line
+    - value
+    - father
 
+ */
+
+const SymbolTableSchema = db.define('SymbolTableSchema', {
+    name: {
+        type: DataTypes.STRING,
+        required: true
+    },
+    dataType: {
+        type: DataTypes.STRING,
+        required: true
+    },
+    type: {
+        type: DataTypes.STRING,
+        required: true
+    },
+    scope: {
+        type: DataTypes.STRING,
+        required: true
+    },
+    line: {
+        type: DataTypes.INTEGER,
+        required: true
+    },
+    value: {
+        type: DataTypes.STRING,
+        required: true
+    },
+    father: {
+        type: DataTypes.STRING,
+        required: true
+    }
+}, {
+    tableName: 'symbolTable'
 });
 
 class SymbolTable {
