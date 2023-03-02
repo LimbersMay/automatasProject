8f756866035d3a3e789d99036bfbecc8d6c1802b
limberMay <limbermay1@gmail.com>
2023-03-02 16:56:57 -0600

    Función para agregar un símbolo a la tabla implementado

---
+++ public/js/api/backend-api.js
@@ -0,0 +1,41 @@
+
+export const API_URL = 'http://localhost:3000';
+
+export const addSymbol = async (name, dataType, type, scope, line, value, father) => {
+
+        const response = await fetch(`${API_URL}/api/save-symbol`, {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                name,
+                dataType,
+                type,
+                scope,
+                line,
+                value,
+                father
+            })
+        });
+
+        const data = await response.json();
+
+        if (response.status !== 200) {
+            return {
+                data: data.message,
+                ok: false
+            }
+        }
+
+        return {
+            data,
+            message: data.message,
+            ok: true
+        }
+}
+
+export const getSymbols = async () => {
+    const data = await fetch(`${API_URL}/api/view`);
+    console.log(data);
+}
