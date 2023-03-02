c865216540ce503f49a9018adc3b27c6dc046f8a
limberMay <limbermay1@gmail.com>
2023-03-01 23:46:22 -0600

    archivo index.js con la configuración básica del servidor creado

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+const express = require('express');
+const app = express();
+
 const db = require('./db/connection');
+const SymbolTableRouter = require('./routes/symbolTable.route');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+db.authenticate().then();
+
+app.use('/api', SymbolTableRouter);
+
+app.listen(3000, () => {
+    console.log('listening on port 3000');
+});
