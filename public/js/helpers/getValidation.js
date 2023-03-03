225d5e12bbbfcfd12f59e0d5cca9fcb4f89636ba
limberMay <limbermay1@gmail.com>
2023-03-03 01:11:06 -0600

    (frontend) error SYMBOL_HAS_CHILDREN agregado al getValidation

--- public/js/helpers/getValidation.js
+++ public/js/helpers/getValidation.js
@@ -5,7 +5,7 @@ export const getValidationError = (error) => {
         SYMBOL_ALREADY_EXISTS: 'Symbol already exists',
         FATHER_DOES_NOT_EXISTS: 'Father does not exists',
         SYMBOL_DOES_NOT_EXISTS: 'Symbol does not exists',
-        SYMBOL_HAS_CHILDREN: 'Symbol has children'
+        SYMBOL_HAS_CHILDREN: 'Symbol has children',
     }
 
     return errors[error] || 'Server error';
